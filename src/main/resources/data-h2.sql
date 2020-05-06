a00be1c7d68867e011ad044616587af883e06f7a
sunny14 <faina.private@gmail.com>
2020-05-06 12:09:02 +0300

    1. right guess -> the answer is found in db -> returns score=10

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,2 +1,6 @@
 INSERT INTO GAME (id, name) VALUES (1, 'TRIVIA1');
 INSERT INTO GAME (id, name) VALUES (2, 'TRIVIA2');
+
+INSERT INTO QUESTION (id, question, game_id, score) VALUES (1, 'what is the answer to life?', 1, 10);
+
+INSERT INTO QUESTION_TO_ANSWER (id, answer, question_id) VALUES (1, '42', 1);
